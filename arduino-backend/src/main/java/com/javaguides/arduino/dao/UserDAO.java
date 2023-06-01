74d161648a76a909382d11c73fa34b3626700034
YShann <luyingshan16@gmail.com>
2023-06-01 21:53:51 +0800

    新增登入功能

--- arduino-backend/src/main/java/com/javaguides/arduino/dao/UserDAO.java
+++ arduino-backend/src/main/java/com/javaguides/arduino/dao/UserDAO.java
@@ -1,7 +1,12 @@
 package com.javaguides.arduino.dao;
 
 import com.javaguides.arduino.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserDAO extends BaseDAO<User, Integer> {
-
+    @Query("FROM User d where d.id = :id")
+    Optional<User> login(@Param("id") Integer id);
 }
--- arduino-backend/src/main/java/com/javaguides/arduino/service/UserService.java
+++ arduino-backend/src/main/java/com/javaguides/arduino/service/UserService.java
@@ -4,6 +4,8 @@ import com.javaguides.arduino.bean.UserBean;
 import com.javaguides.arduino.dao.UserDAO;
 import com.javaguides.arduino.entity.User;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.DigestUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,11 +21,14 @@ public class UserService {
     }
 
     public void save(UserBean userBean) {
-        User saveResult = userDAO.saveAndFlush(convertBeanToEntity(userBean));
+        User user = convertBeanToEntity(userBean);
+        User saveResult = userDAO.saveAndFlush(user);
+        String md5Password = DigestUtils.md5DigestAsHex(userBean.password.getBytes());
+        saveResult.setPassword(md5Password);
         userDAO.save(saveResult);
     }
 
-    public List<UserBean> searchAll(){
+    public List<UserBean> searchAll() {
         return userDAO.findAll()
                 .stream()
                 .map(this::convertEntityToBean)
@@ -41,12 +46,24 @@ public class UserService {
         }
     }
 
+    public Optional<UserBean> login(Integer id, String password) {
+        Optional<User> optional = userDAO.login(id);
+        User entity = optional.get();
+        UserBean bean = convertEntityToBean(entity);
+        String md5Password = DigestUtils.md5DigestAsHex(password.getBytes());
+        if (!bean.password.equals(md5Password)) {
+            bean = new UserBean();
+        }
+        return Optional.of(bean);
+    }
+
     public void update(UserBean userBean) {
         Optional<User> optional = userDAO.findById(userBean.getId());
         User user = optional.get();
         user.setName(userBean.getName());
         user.setEmail(userBean.getEmail());
-        user.setPassword(userBean.getPassword());
+        String md5Password = DigestUtils.md5DigestAsHex(userBean.password.getBytes());
+        user.setPassword(md5Password);
         userDAO.update(user);
     }
 
@@ -54,7 +71,7 @@ public class UserService {
         userDAO.deleteById(id);
     }
 
-    private UserBean convertEntityToBean(User user){
+    private UserBean convertEntityToBean(User user) {
         UserBean userBean = new UserBean();
         userBean.setId(user.getId());
         userBean.setName(user.getName());
@@ -63,7 +80,7 @@ public class UserService {
         return userBean;
     }
 
-    private User convertBeanToEntity(UserBean userBean){
+    private User convertBeanToEntity(UserBean userBean) {
         User user = new User();
         user.setId(userBean.getId());
         user.setName(userBean.getName());
